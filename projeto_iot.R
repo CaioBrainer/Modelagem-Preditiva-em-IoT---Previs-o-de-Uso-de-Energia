3479ec1d6a58831afe62a0b07fe010b30c82aa7c
Caio Brainer <caiocgbrainer@gmail.com>
2022-09-28 20:01:18 -0300

    'att'

--- projeto_iot.R
+++ projeto_iot.R
@@ -4,7 +4,7 @@
 
 
 #Setando o diretório do projeto
-setwd("machine_learning/Datasets e projetos/Projetos-7-8/Projeto 8/")
+#setwd("machine_learning/Datasets e projetos/Projetos-7-8/Projeto 8/Modelagem_Preditiva_em_IoT/")
 getwd()
 
 #carregando os pacotes a serem utilizados
@@ -13,6 +13,7 @@ library("dplyr")
 library("tidyr")
 library('ggplot2')
 library('corrplot')
+library('lubridate')
 
 #para descarrecar o pacote:
 'detach(package:tidyr, unload=TRUE)'
@@ -20,8 +21,8 @@ library('corrplot')
 
 #Carregando os dados
 
-df_treino <- read.csv("Modelagem_Preditiva_em_IoT/projeto8-data_files/projeto8-training.csv") 
-df_teste <- read.csv("Modelagem_Preditiva_em_IoT/projeto8-data_files/projeto8-testing.csv")
+df_treino <- read.csv("projeto8-data_files/projeto8-training.csv") 
+df_teste <- read.csv("projeto8-data_files/projeto8-testing.csv")
 
 
 #Verificando o dataset e os tipos de dados apresentados pelo dataset
@@ -29,8 +30,10 @@ df_teste <- read.csv("Modelagem_Preditiva_em_IoT/projeto8-data_files/projeto8-te
 View(df_treino)
 View(df_teste)
 
-'Observa se que o dataset é composto majoritariamente por variáveis numéricas,
-somente duas variáveis são caracteres: Day_of_week e WeekStatus'
+#Observa se que o dataset é composto majoritariamente por variáveis numéricas,
+#somente duas variáveis são caracteres: Day_of_week e WeekStatus
+
+summary(df_treino)
 
 #----------------------ANÁLISE EXPLORATÓRIA DOS DADOS-------------------------#
 
@@ -52,10 +55,20 @@ plot1 <- ggplot(df_num_long, aes(x = value)) + #Plota histogramas individuais
   facet_wrap( ~ name, scales = "free") #scale = "free" gera eixos independentes
 
 plot1
+#Os dados numéricos apresentam uma distribuição aproximadamente normal para a
+#maioria das variáveis, com exceção das variáveis randômicas introduzidas no 
+#dataset e de algumas outras variáveis como a appliance, lights e RH_out
 
-'Os dados numéricos apresentam uma distribuição aproximadamente normal para a
-maioria das variáveis, com exceção das variáveis randômicas introduzidas no 
-dataset e de algumas outras variáveis como a appliance, lights e RH_out'
+#Visualizando box-plots para observar valores outliers
+
+plot2 <- ggplot(df_num_long, aes(x = value)) + 
+  geom_boxplot() +
+  facet_wrap( ~ name, scales = "free")
+
+plot2
+
+
+
 
 #Observando a correlação entre as variáveis numéricas
 #Obs: a correlação analisa apenas correlação entre variáveis numéricas
@@ -63,13 +76,13 @@ dataset e de algumas outras variáveis como a appliance, lights e RH_out'
 cor_df <- cor(df_num)
 cor_df
 
-'Podemos perceber que o consumo (variável Appliances) apresenta baixos 
-níveis de correlação com as variáveis light, T2, T6, T_out, RH_out e NSM.
-As demais variáveis apresentam correlações abaixo de 0.10/-0.10'
+#Podemos perceber que o consumo (variável Appliances) apresenta baixos 
+#níveis de correlação com as variáveis light, T2, T6, T_out, RH_out e NSM.
+#As demais variáveis apresentam correlações abaixo de 0.10/-0.10
 
 #visualizando a matriz de correlação de forma gráfica para facilitar
 
-plot2 <- corrplot(cor_df, method = 'color')
+corrplot(cor_df, method = 'color')
 
 
 
@@ -85,27 +98,35 @@ consumo_por_dia <- df_treino %>%
 
 consumo_por_dia
 
-plot2 <- ggplot(consumo_por_dia, aes(x=as.factor(Day_of_week), y=consumo)) + 
-  geom_bar(stat = "identity", color='black', fill='blue')
+plot3 <- ggplot(consumo_por_dia, aes(x=Day_of_week, y=consumo)) + 
+  geom_bar(stat = "identity", color='black')
 
-plot2
+plot3
 
-'Observamos que os dias de sexta a segunda-feira temos um maior consumo 
-de energia'
 
+#Observamos que os dias de sexta a segunda-feira temos um maior consumo 
+#de energia
+
+#Consumo baseado em horas
 
 #Função para extrair informações de datas -> 
 #as.POSIXct(dates, format = "%m/%d/%Y %H:%M:%S")
 
-df_treino$horario <- format(as.POSIXct(df_treino$date), format = "%H:%M")
-df_treino$mes <- format(as.POSIXct(df_treino$date), format = "%m")
+#ficam como string
+# df_treino$horario <- format(as.POSIXct(df_treino$date), format = "%H:%M")
+# df_treino$mes <- format(as.POSIXct(df_treino$date), format = "%m")
+
+df_treino$horario <- hour(df_treino$date)
+df_treino$mes <- month(df_treino$date)
+
+plot4 <- ggplot(df_treino, aes(x=horario2, y=mean(Appliances))) + 
+  geom_bar(stat = "identity", color='black')
+
+plot4
+
 
 #parei aqui
 
-as.integer(df_treino$mes)
-typeof(df_treino$mes)
-as.integer(df_treino$horario)
-typeof(df_treino$horario)
 
 
 
