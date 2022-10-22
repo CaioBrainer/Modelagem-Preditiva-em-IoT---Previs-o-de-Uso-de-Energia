b0b6ecf7541ebb532878648091d7522093449b44
Caio Brainer <caiocgbrainer@gmail.com>
2022-10-21 22:34:22 -0300

    script_atualizado

--- projeto_iot.R
+++ projeto_iot.R
@@ -16,24 +16,99 @@ getwd()
 #carregando os pacotes a serem utilizados
 
 library(dplyr)
-library(tidyr)
 library(ggplot2)
 library(corrplot)
 library(party)
 library(Metrics)
 library(CatEncoders)
-library(caret)
+
 library(performance)
 library(psych)
+library(lubridate)
+
+library(caret)
+library(randomForest)
+library(gbm)
+library(xgboost)
+
+#----------------------------------FUNÇÕES-------------------------------------#
+#Nesta parte estarão funções a serem utilizadas em diversos momentos 
+
+avaliacaoModelo <- function(nome_do_modelo, observado, predito){
+  
+  
+  data.frame(Modelo = as.character(nome_do_modelo),
+             RMSE = RMSE(predito, observado),
+             MAE = MAE(predito, observado),
+             R_Square = R2(predito, observado),
+             check.names = F
+  ) %>% 
+    mutate(MSE = sqrt(RMSE))
+}
+
+#pipeline para gerar um dataframe com variáveis modificadas
+modificaColunas <- function(dataframe_iot) {
+  
+  #gerando as colunas T_media e RH_media
+  df_modificado = dataframe_iot %>%
+    mutate("T_media" = rowMeans(select(dataframe_iot,c(T1,T2,T3,T4,T5
+                                                       ,T6,T7,T8,T9)))) %>%
+    mutate("RH_media" = rowMeans(select(dataframe_iot,c(RH_1,RH_2,RH_3,RH_4,RH_5
+                                                        ,RH_6,RH_7,RH_8,RH_9)))) %>%
+    select(-c(date,rv1,rv2, T1:RH_9))
+  
+  #escalonando as variáveis numéricas
+  pipeline_mod = preProcess(df_modificado[,2:11], method = c("center", "scale"))
+  df_modificado = predict(pipeline_mod, df_modificado[,1:11])
+  
+  #tratando variáveis categóricas
+  labels_Weekstatus <- LabelEncoder.fit(df_modificado$WeekStatus)
+  df_modificado$WeekStatus <- transform(labels_Weekstatus, as.character(
+    df_modificado$WeekStatus))
+  
+  labels_DayofWeek <- LabelEncoder.fit(df_treino$Day_of_week)
+  df_modificado$Day_of_week <- transform(labels_DayofWeek, as.character(
+    df_modificado$Day_of_week))
+  
+  return(df_modificado)
+  
+  
+}
+
+#Pipeline para processar variáveis numéricas e categoricas no dataframe completo
+pipelineProcessamento <- function(dataframe_iot) {
+  
+  dataframe_iot = dataframe_iot
+  
+  
+    #escalonando as variáveis numéricas
+    preProcessamento <- preProcess(dataframe_iot[,3:32], method = c("center", "scale"))
+    dataframe_iot <- predict(preProcessamento, dataframe_iot[,2:32])
+    
+    labels_Weekstatus <- LabelEncoder.fit(dataframe_iot$WeekStatus)
+    dataframe_iot$WeekStatus <- transform(labels_Weekstatus, as.character(
+      dataframe_iot$WeekStatus))
+    
+    labels_DayofWeek <- LabelEncoder.fit(dataframe_iot$Day_of_week)
+    dataframe_iot$Day_of_week <- transform(labels_DayofWeek, as.character(
+      dataframe_iot$Day_of_week))
+  
+    return(dataframe_iot)
+} #ok
+
 
 #------------------------------------------------------------------------------#
 #---------------------------CARREGANDO OS DADOS--------------------------------#
 
 #Os dados fornecidos já estão divididos entre treino e teste:
 
-df_treino <- read.csv("projeto8-data_files/projeto8-training.csv") 
-df_teste <- read.csv("projeto8-data_files/projeto8-testing.csv")
+df_treino <- read.csv("projeto8-data_files/projeto8-training.csv")
+str(df_treino) #14803
 
+df_teste <- read.csv("projeto8-data_files/projeto8-testing.csv")
+str(df_teste) #4932
+
+df_completo <- rbind(df_treino, df_teste)
 
 #Verificando o dataset e os tipos de dados apresentados pelo dataset
 
@@ -86,15 +161,20 @@ plot1
 #-----------------------------------------------------------------------------#
 #         Visualizando box-plots para observar valores outliers
 
+
 plot2 <- ggplot(df_num_long, aes(x = value)) + 
   geom_boxplot() +
   facet_wrap( ~ name, scales = "free")
 
 plot2
 
-#Em vista que a variável preditora apresenta muitos valores outliers, iremos
-#filtrar o consumo para < 150 KW / através do Z score
 
+#-----------------------------------------------------------------------------#
+boxplot(df_treino$Appliances, col='blue')
+min(boxplot.stats(df_treino$Appliances)$out)
+
+#Em vista que a variável preditora apresenta muitos valores outliers, iremos
+#filtrar o consumo para < 200 KW 
 
 df_treino <- df_treino %>% filter(Appliances < 200)
 
@@ -106,9 +186,9 @@ df_treino <- df_treino %>% filter(Appliances < 200)
 #-----------------------------------------------------------------------------#
 #         Observando a correlação entre as variáveis numéricas
 
+
 cor_df <- cor(df_num)
 cor_df
-
 
 
 #Podemos perceber que o consumo (variável Appliances) apresenta baixos 
@@ -144,9 +224,6 @@ plot3
 
 
 #Consumo baseado em horas
-library(lubridate)
-
-
 #Função para extrair informações de datas -> 
 #as.POSIXct(dates, format = "%m/%d/%Y %H:%M:%S")
 
@@ -170,8 +247,8 @@ plot4
 
 
 ?preProcess
-pipeline <- preProcess(df_treino[,3:34], method = c("center", "scale"))
-df_treino_esc<- predict(pipeline, df_treino[,2:34])
+preProcessamento <- preProcess(df_treino[,3:34], method = c("center", "scale"))
+df_treino_esc<- predict(preProcessamento, df_treino[,2:34])
 
 
 #                           VARIÁVEIS CATEGÓRICAS
@@ -194,7 +271,7 @@ df_treino_esc$Day_of_week <- transform(labels2, as.character(df_treino$Day_of_we
 #Usaremos a biblioteca random forest para visualizar os atributos com maior
 #importância para a predição de valores de consumo
 
-library(randomForest)
+
 ?randomForest
 random_forest <- randomForest(Appliances ~ . ,data = df_treino_esc, ntree = 500,
                               importance= TRUE)
@@ -244,7 +321,7 @@ df_teste_esc$Day_of_week <- transform(labels2, as.character(df_teste$Day_of_week
 View(df_teste_esc)
 
 #------------------------------------------------------------------------------#
-#--------------- --TREINANDO UM MODELO REGRESSÃO LINEAR -----------------------#
+#------------------TREINANDO UM MODELO REGRESSÃO LINEAR -----------------------#
 
 
 lin_reg_v1 <- lm(Appliances ~ lights + T3 + T6 + T7 + T8 + T9 + T_out +
@@ -257,31 +334,16 @@ lin_reg_v1 <- lm(Appliances ~ lights + T3 + T6 + T7 + T8 + T9 + T_out +
 previsões_rin_reg <- predict(lin_reg_v1, df_teste_esc[,2:33])
 summary(lin_reg_v1)
 
-avaliacao_modelo <- function(nome_do_modelo, observado, predito){
-  
-  
-  data.frame(Modelo = as.character(nome_do_modelo),
-             RMSE = RMSE(predito, observado),
-             MAE = MAE(predito, observado),
-             R_Square = R2(predito, observado),
-             check.names = F
-  ) %>% 
-    mutate(MSE = sqrt(RMSE))
-}
-
 avaliacao_rl <- avaliacao_modelo("regressão linear", df_teste_esc$Appliances,
                                  previsões_rin_reg)
 
 avaliacao_rl
 
-
+saveRDS(lin_reg_v1, "lin_reg_V1.rds")
 
 #------------------------------------------------------------------------------#
 #-------------------TREINANDO UM MODELO DE GRADIENTE BOOSTING------------------#
 
-
-
-library("gbm")
 
 ?gbm
 grad_bst <- gbm(Appliances ~ lights + NSM + RH_1 + RH_2 + RH_3 + RH_5 + T6 + T7 + T8 
@@ -302,50 +364,19 @@ MAE(previsões_gb, df_teste_esc[,1]) #12.63
 
 saveRDS(grad_bst, "grad_bst_v1.rds")
 
-?R2
-
-#------------------------------------------------------------------------------#
-library("h2o")
-
-
-h2o.init()
-dependente <- "Appliances"
-
-independentes <- c("lights", "NSM", "RH_1", "RH_2", "RH_5", "T6", "T7", "T8", 
-                   "T9", "T_out", "Press_mm_hg", "Visibility", "Tdewpoint")
-
-df_h2o <- as.h2o(df_treino_esc)
-df_h2o_teste <- as.h2o(df_teste_esc)
-
-
-?h2o.gbm
-grad_bst <- h2o.gbm(y=dependente, x=independentes, 
-                    training_frame = df_h2o)
-
-
-
-#avaliação
-h2o.rmse(grad_bst) #15.81
-h2o.mse(grad_bst) #250.15
-h2o.mae(grad_bst) #11.75
-
-performance_gbm <- h2o.performance(grad_bst, newdata = df_h2o_teste)
-performance_gbm
-
-#detach(package="h2o", unload = T)
-
 
 #------------------------------------------------------------------------------#
 #--------------------------TREINANDO UM MODELO DE XGboost----------------------#
 
-library("xgboost")
 
 ?xgboost
 
 parametros <- list(eta = 0.1, subsample = 0.5, max_depth=6) #default
 
-X <- as.matrix(df_treino_esc[c("lights", "NSM", "RH_1", "RH_2", "RH_3", "RH_5", "T6", "T7", "T8", 
-                               "T9", "T_out", "Press_mm_hg", "Visibility", "Tdewpoint")])
+X <- as.matrix(df_treino_esc[c("lights", "NSM", "RH_1", "RH_2", "RH_3",
+                               "RH_5", "T6", "T7", "T8", "T9", "T_out", 
+                               "Press_mm_hg", "Visibility", "Tdewpoint")])
+
 y <- as.matrix(df_treino_esc[,1])
 
 #modelo com todos as variáveis
@@ -357,8 +388,10 @@ xtreme_bst_v1 <- xgboost(data = X, label = y,
 
 plot(xtreme_bst_v1$evaluation_log, type='l', col='blue')
 
-X_teste <- as.matrix(df_teste_esc[c("lights", "NSM", "RH_1", "RH_2", "RH_3", "RH_5", "T6", "T7", "T8", 
-                                    "T9", "T_out", "Press_mm_hg", "Visibility", "Tdewpoint")])
+X_teste <- as.matrix(df_teste_esc[c("lights", "NSM", "RH_1", "RH_2", "RH_3", 
+                                    "RH_5", "T6", "T7", "T8","T9", "T_out",
+                                    "Press_mm_hg", "Visibility", "Tdewpoint")])
+
 y_teste <- as.matrix(df_teste_esc[,1])
 
 previsoes_xgb <- predict(xtreme_bst_v1, X_teste)
@@ -369,48 +402,20 @@ avaliacao_xgb <- avaliacao_modelo('XGBoost',as.numeric(df_teste_esc$Appliances),
 
 saveRDS(xtreme_bst_v1, "xtreme_bst_v1.rds")
 
-#parei aqui
+#------------------------------------------------------------------------------#
+#--------------------------SALVANDO RESULTADOS---------------------------------#
 
 ?cbind
 resultados_metricas <- rbind(avaliacao_rl, avaliacao_gbm, avaliacao_xgb)
 View(resultados_metricas)
 
 write.csv(resultados_metricas, file="resultados_modelos.csv")
+
 results <- read.csv(file="resultados_modelos.csv", row.names = 1)
 
 View(resultss)
 #------------------------------------------------------------------------------#
 #                                IDEIAS
-
-
-df_treino2 <- df_treino %>%
-  mutate("T_media" = rowMeans(select(df_treino,c(T1,T2,T3,T4,T5
-                                                 ,T6,T7,T8,T9)))) %>%
-  mutate("RH_media" = rowMeans(select(df_treino,c(RH_1,RH_2,RH_3,RH_4,RH_5
-                                                 ,RH_6,RH_7,RH_8,RH_9)))) %>%
-  select(-c(date, T1:RH_9))
-
-
-#para limpar os outliers de maneira mais efetiva!
-
-#esse método é melhor (critério interquartil)
-boxplot(df_treino$Appliances, col='blue')
-min(boxplot.stats(df_treino$Appliances)$out)
-
-#esse método leva a modelos mais fracos
-data_clean <- data_scale %>% 
-  mutate( zscore = (Appliances - mean(Appliances)) / sd(Appliances)) %>%
-  filter(zscore <=3) %>%
-  select(-zscore)
-
-
-
-
-
-
-
-
-
 
 
 
